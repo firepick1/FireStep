227fa36c4da7754a064b6bd3ebdf798478bf8dcc
firepick1 (pixel) <karl@firepick.org>
2015-05-01 09:53:45 -0700

    version.h

---
+++ FireStep/version.h
@@ -0,0 +1,22 @@
+#ifndef VERSION_H
+#define VERSION_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//////////// GENERATED FILE (from version.h.in) /////////////////
+//////////// GENERATED FILE (from version.h.in) /////////////////
+//////////// GENERATED FILE (from version.h.in) /////////////////
+
+#define VERSION_MAJOR 0
+#define VERSION_MINOR 1
+#define VERSION_PATCH 2
+
+//////////// GENERATED FILE (from version.h.in) /////////////////
+//////////// GENERATED FILE (from version.h.in) /////////////////
+//////////// GENERATED FILE (from version.h.in) /////////////////
+
+#ifdef __cplusplus
+}
+#endif
+#endif
