9800bdc0ac13a2b25d68d91d741912547876cd5c
Karl Lew <karl@firepick.org>
2015-06-02 20:47:15 -0700

    digitalWrite

--- FireStep/Machine.h
+++ FireStep/Machine.h
@@ -167,6 +167,33 @@ typedef class Axis {
 typedef int8_t AxisIndex;
 typedef int8_t MotorIndex;
 
+inline void digitalWriteFast(uint8_t pin, uint8_t val)
+{
+	//uint8_t timer = digitalPinToTimer(pin);
+	uint8_t bit = digitalPinToBitMask(pin);
+	uint8_t port = digitalPinToPort(pin);
+	volatile uint8_t *out;
+
+	//if (port == NOT_A_PIN) return;
+
+	// If the pin that support PWM output, we need to turn it off
+	// before doing a digital write.
+	//if (timer != NOT_ON_TIMER) turnOffPWM(timer);
+
+	out = portOutputRegister(port);
+
+	uint8_t oldSREG = SREG;
+	cli();
+
+	if (val == LOW) {
+		*out &= ~bit;
+	} else {
+		*out |= bit;
+	}
+
+	SREG = oldSREG;
+}
+
 typedef class Machine : public QuadStepper {
         friend void ::test_Home();
     private:
@@ -189,9 +216,9 @@ typedef class Machine : public QuadStepper {
         void enable(bool active);
         virtual Status step(const Quad<StepDV> &pulse);
 		inline void pulsePin1(int16_t pinStep) {
-			digitalWrite(pinStep, HIGH);
+			digitalWriteFast(pinStep, HIGH);
 			PULSE_WIDTH_DELAY();
-			digitalWrite(pinStep, LOW);
+			digitalWriteFast(pinStep, LOW);
 		}
 		inline int8_t pulsePin(int16_t pinStep, int8_t n) {
 			switch (n) {
