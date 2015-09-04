c7ce4535dd5be34fade250d96ec60f7df0a71a3d
firepick1 (localhost) <karl@firepick.org>
2015-09-04 06:37:57 -0700

    version

--- src/firestep.cpp
+++ src/firestep.cpp
@@ -19,8 +19,7 @@
 using namespace firestep;
 
 int help(int rc=0) {
-    cerr << "HELP	: firestep command line client v"
-         << VERSION_MAJOR <<"."<< VERSION_MINOR <<"."<< VERSION_PATCH << endl;
+    cerr << "HELP	: " << FireStepClient::version() << endl;
 	cerr << "HELP	:" << endl;
 	cerr << "HELP	: EXAMPLES" << endl;
 	cerr << "HELP	:   Launch interactive console with prompt" << endl;
@@ -37,149 +36,11 @@ int help(int rc=0) {
 	cerr << "HELP	:   Show this help text" << endl;
 	cerr << "HELP	:     firestep -h" << endl;
 	cerr << "HELP	:     firestep --help" << endl;
+	cerr << "HELP	:   Show concise version " << endl;
+	cerr << "HELP	:     firestep -v" << endl;
+	cerr << "HELP	:     firestep --version" << endl;
     return rc;
 }
-
-#ifdef tbd
-typedef class FireStepClient {
-private:
-    bool prompt;
-    string serialPath;
-    int32_t msResponse; // response timeout
-    ArduinoUSB usb;
-public:
-    FireStepClient(bool prompt=true, const char *serialPath=SERIAL_PATH, int32_t msResponse=10*1000);
-    int reset();
-    int console();
-    int sendJson(string json);
-} FireStepClient;
-
-FireStepClient::FireStepClient(bool prompt, const char *serialPath, int32_t msResponse)
-    : prompt(prompt),serialPath(serialPath), msResponse(msResponse), usb(serialPath)
-{
-}
-
-int FireStepClient::reset() {
-    int rc = 0;
-    if (usb.isOpen()) {
-        rc = usb.close();
-    }
-    if (rc == 0) {
-        rc = usb.configure(true);
-    }
-    if (rc != 0) {
-        cerr << "ERROR	: could not configure serial port " << serialPath << endl;
-        LOGERROR("FireStepClient::reset(hup) failed");
-        return rc;
-    }
-
-    rc = usb.open();
-    if (rc==0) { // clear out startup text
-        string ignore = usb.readln(5000);
-        while (ignore.size()) {
-            LOGINFO1("FireStepClient::reset() start:%s", ignore.c_str());
-            if (prompt) {
-                cerr << "START	: " << ignore;
-            }
-            ignore = usb.readln();
-        }
-    }
-
-    if (rc == 0) {
-        usb.close();
-    }
-    if (rc == 0) {
-        rc = usb.configure(false);
-    }
-    if (rc != 0) {
-        cerr << "ERROR	: could not configure serial port " << serialPath << endl;
-        LOGERROR("FireStepClient::reset(nohup) failed");
-        return rc;
-	}
-	return rc;
-}
-
-int FireStepClient::sendJson(string request) {
-    int rc = usb.open();
-    if (rc != 0) {
-        return rc;
-    }
-
-    usb.writeln(request);
-    LOGDEBUG2("sendJson() bytes:%ld write:%s", (long) request.size(), request.c_str());
-
-    string response = usb.readln(msResponse);
-    for (int i=0; response.size()==0 && i<4; i++) {
-        LOGDEBUG1("sendJson() wait %ldms", (long) msResponse);
-        response = usb.readln(msResponse);
-    }
-    if (response.size() > 0) {
-        cout << response;
-        LOGDEBUG1("sendJson() read:%s", response.c_str());
-    } else {
-        LOGERROR("sendJson() timeout");
-        return -ETIME;
-    }
-
-    return usb.close();
-}
-
-int FireStepClient::console() {
-    int rc = usb.open();
-    if (rc != 0) {
-        return rc;
-    }
-
-    for (;;) {
-        if (prompt) {
-            cerr << "CMD	: ";
-        }
-        string request;
-        cin >> request;
-        if (request == "quit") {
-            if (prompt) {
-                cerr << "OK	: quit" << endl;
-            }
-            LOGDEBUG("console() quit");
-            break;
-        } else if (request.size() > 0) {
-            usb.writeln(request);
-            LOGDEBUG2("console() bytes:%ld write:%s", (long) request.size(), request.c_str());
-        } else { // EOF for file stdin
-            if (prompt) {
-                cerr << "EOF" << endl;
-            }
-            LOGDEBUG("console() EOF");
-            break;
-        }
-        string response = usb.readln(msResponse);
-        for (int i=0; response.size()==0 && i<4; i++) {
-            if (prompt) {
-                cerr << "STATUS	: wait " << msResponse << "ms..." << endl;
-            }
-            LOGDEBUG1("console() wait %ldms", (long) msResponse);
-            response = usb.readln(msResponse);
-        }
-        if (response.size() > 0) {
-            cout << response;
-            LOGDEBUG1("console() read:%s", response.c_str());
-        } else {
-            if (prompt) {
-                cerr << "ERROR	: timeout" << endl;
-            }
-            LOGERROR("console() timeout");
-            break;
-        }
-    }
-
-    if (prompt) {
-        cout << "END	: closing serial port" << endl;
-    }
-    LOGINFO("console() closing serial port");
-
-    return usb.close();
-}
-#endif
 
 int parse_args(int argc, char *argv[], bool &prompt, bool &logging, string &json, bool& reset) {
     int rc = 0;
@@ -193,6 +54,9 @@ int parse_args(int argc, char *argv[], bool &prompt, bool &logging, string &json
                 return help(-EINVAL);
             }
             json = argv[iArg];
+        } else if (strcmp("-v",argv[iArg])==0 || strcmp("--version", argv[iArg])==0) {
+			cerr << FireStepClient::version(false) << endl;
+			return -EAGAIN;
         } else if (strcmp("-r",argv[iArg])==0 || strcmp("--reset", argv[iArg])==0) {
             reset = true;
         } else if (strcmp("-p",argv[iArg])==0 || strcmp("--prompt", argv[iArg])==0) {
@@ -221,11 +85,6 @@ int parse_args(int argc, char *argv[], bool &prompt, bool &logging, string &json
 }
 
 int main(int argc, char *argv[]) {
-    char ver[100];
-    snprintf(ver, sizeof(ver), "firestep command line client v%d.%d.%d",
-             VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH);
-    LOGINFO1("%s", ver);
-
     bool prompt = false;
     bool reset = false;
     bool logging = false;
@@ -235,15 +94,18 @@ int main(int argc, char *argv[]) {
         return rc;
     }
 
+	FireStepClient fsc(prompt);
+
     if (prompt) {
-        cerr << "STATUS	: " << ver << endl;
+        cerr << "STATUS	: " << FireStepClient::version() << endl;
     }
 
-    FireStepClient fsc(prompt);
     if (rc == 0) {
 		if (reset) {
 			rc = fsc.reset();
-		} else if (json.size()) {
+		} 
+		
+		if (json.size()) {
             rc = fsc.sendJson(json.c_str());
         } else {
             rc = fsc.console();
